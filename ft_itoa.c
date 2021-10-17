24f536e8d890ba14a2249b0c828b9815ba70be7d
Eldridge Raynald <eraynald@ox-n1.msk.21-school.ru>
2021-10-17 16:08:12 +0300

    last version

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,76 +6,52 @@
 /*   By: eraynald <eraynald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 17:17:57 by eraynald          #+#    #+#             */
-/*   Updated: 2021/10/13 17:17:20 by eraynald         ###   ########.fr       */
+/*   Updated: 2021/10/17 16:03:23 by eraynald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	step(int len)
+int	schet_len(int n)
 {
-	int	i;
+	int	len;
 
-	i = 1;
-	while (--len > 0)
-		i *= 10;
-	return (i);
-}
-
-char	*in_len(int trig, int n, int len, char *p, int trig_min_int)
-{
-	int	i;
-
-	i = 0 + trig;
-	if (trig == 1)
+	if (n <= 0)
+		len = 1;
+	else
+		len = 0;
+	while (n != 0)
 	{
-		p[0] = '-';
-		if (trig_min_int == 1)
-			n = n*-1 -1;
-		else
-			n *= -1;
+		len++;
+		n /= 10;
 	}
-	if (n == 0) 
-		return ("0\0");
-	while (i < len + trig)
-	{
-		p[i] = n / step(len - i + trig) + 48;
-		n %= step(len - i + trig);
-		i++;
-	}
-	if (trig_min_int == 1)
-		p[i - 1] += 1;
-	p[len + trig] = '\0';
-	return (p);
+	return (len);
 }
 
 char	*ft_itoa(int n)
 {
-	int			len;
-	long int	i;
-	char		*p;
-	int			trig;
-	int			trig_min_int;
+	char	*p;
+	int		len;
+	int		negativ;
 
-	trig_min_int = 0;
-	if (n == -2147483648)
-		trig_min_int = 1;
-	trig = 0;
 	if (n < 0)
-		trig = 1;
-	len = 0;
-	i = n;
-	while (i != 0)
-	{
-		len++;
-		i = i / 10;
-	}
-	if (n == 0)
-		p = malloc(sizeof(char) * 2);
+		negativ = -1;
 	else
-		p = malloc(sizeof(char) * (len + 1 + trig));
+		negativ = 1;
+	len = schet_len(n);
+	p = (char *)malloc(sizeof(char) * len + 1);
 	if (p == NULL)
-		return (NULL);
-	p = in_len(trig, n, len, p, trig_min_int);
+		return (0);
+	p[len] = '\0';
+	while (--len >= 0)
+	{
+		if (n < 0)
+			p[len] = n % 10 * -1 + '0';
+		else
+			p[len] = n % 10 + '0';
+		n /= 10;
+	}
+	if (negativ == -1)
+		p[0] = '-';
 	return (p);
 }
